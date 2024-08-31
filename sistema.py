c8fd9fdef57d61713339ee2f28c3c7698850de53
thassio <thassio@gmail.com>
2024-08-30 21:36:16 -0300

    second: print corrigido

--- sistema.py
+++ sistema.py
@@ -32,30 +32,34 @@ else:
                 print("Favor depositar quantia positiva")
             else:
                 saldo += movi
-                print(f"Deposito de R${movi}")
+                print(f"Deposito de R${movi:.2f}")
                 extrat += f"\n.D +{movi}"
 
         elif op == "S":
             if qtd_saque > 0:
                 movi = float(input())
                 if movi <= 500 and movi <= saldo:
-                    print(f"Saldo anterior:{saldo}")
+                    print(f"Saldo anterior:{saldo:.2f}")
                     saldo -= movi
-                    print(f"Saldo atualizado:{saldo}")
+                    print(f"Saldo atualizado:{saldo:.2f}")
                     qtd_saque -= 1
                     print(f"Restam {qtd_saque} saques")
-                    extrat += f"\n.S -{movi}"
+                    extrat += f"\n.S -{movi:.2f}"
                 else:
                     if movi > saldo:
                         print("Saldo insuficiente")
+                        print(f"Saldo atual: R${saldo:.2f}")
                     if movi > 500:
                         print("Limite por saque de: R$500,00")
             else:
                 print("Limite de quantidade de saque atingido")
 
         elif op == "E":
-            print(extrat)
-            print(f"Saldo: {saldo}")
+            if len(extrat)>0:
+                print(extrat)
+                print(f"Saldo: R${saldo:.2f}")
+            else:
+                print("Não foram realizadas movimentações")
         else:
             print("Operacao inválida")
         
